00c2691992574a0c1bdb0502566580e6776ee077
Jeongsu Han <ggoatop98@gmail.com>
2025-07-18 15:33:56 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <TableHead classNa

--- src/components/students/StudentList.tsx
+++ src/components/students/StudentList.tsx
@@ -39,7 +39,7 @@ export default function StudentList({ students, onEdit, onDelete, onAddLog, load
               <TableHead>이메일</TableHead>
               <TableHead>상담분야</TableHead>
               <TableHead className="text-center">상담일지</TableHead>
-              <TableHead className="text-right">작업</TableHead>
+              <TableHead className="text-center">작업</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
@@ -52,7 +52,7 @@ export default function StudentList({ students, onEdit, onDelete, onAddLog, load
                 <TableCell><Skeleton className="h-4 w-40" /></TableCell>
                 <TableCell><Skeleton className="h-4 w-24" /></TableCell>
                 <TableCell><Skeleton className="h-8 w-24 mx-auto" /></TableCell>
-                <TableCell className="text-right"><Skeleton className="h-8 w-20 ml-auto" /></TableCell>
+                <TableCell className="text-center"><Skeleton className="h-8 w-20 mx-auto" /></TableCell>
               </TableRow>
             ))}
           </TableBody>
@@ -73,7 +73,7 @@ export default function StudentList({ students, onEdit, onDelete, onAddLog, load
             <TableHead>이메일</TableHead>
             <TableHead>상담분야</TableHead>
             <TableHead className="text-center">상담일지</TableHead>
-            <TableHead className="text-right w-[120px]">작업</TableHead>
+            <TableHead className="text-center w-[120px]">작업</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
@@ -102,7 +102,7 @@ export default function StudentList({ students, onEdit, onDelete, onAddLog, load
                     기록하기
                   </Button>
                 </TableCell>
-                <TableCell className="text-right">
+                <TableCell className="text-center">
                   <Button variant="ghost" size="icon" onClick={() => onEdit(student)} title="수정">
                     <Pencil className="h-4 w-4" />
                     <span className="sr-only">수정</span>
