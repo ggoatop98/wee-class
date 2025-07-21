fe1293100e3412f81723d28fc9591c33e20ec698
Jeongsu Han <ggoatop98@gmail.com>
2025-07-21 11:36:16 +0000

    사례개념화 작성 창의 "취소","저장" 버튼이 너무 오른쪽 위의 구석에 있어. 위치를 지나치게 구석에 위치하지 않도록 수정해줘.

--- src/components/records/CaseConceptualizationForm.tsx
+++ src/components/records/CaseConceptualizationForm.tsx
@@ -23,14 +23,14 @@ export default function CaseConceptualizationForm({ studentName, initialContent,
     };
 
     return (
-        <div className="flex flex-col h-screen">
+        <div className="flex flex-col h-screen p-8">
              <PageHeader title={`${studentName} 사례개념화 ${initialContent ? '수정' : '작성'}`}>
                 <div className="flex gap-2">
                     <Button variant="outline" onClick={onCancel}>취소</Button>
                     <Button onClick={handleSaveClick}>저장</Button>
                 </div>
             </PageHeader>
-            <div className="flex-grow p-8 pt-0">
+            <div className="flex-grow pt-0">
                 <RichTextEditor
                     content={content}
                     onChange={setContent}
