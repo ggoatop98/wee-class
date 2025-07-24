e7570c6633d1ff654cbaeb855bd8fb21dac2283b
Jeongsu Han <ggoatop98@gmail.com>
2025-07-24 11:46:47 +0000

    studio-8656207586:~/studio{master}$ gsutil cors set cors.json gs://holy-

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCzS0kGNOaEcKKAWN5Bhk37W8c8i7aZghs",
   authDomain: "holy-counseling-aid.firebaseapp.com",
   projectId: "holy-counseling-aid",
-  storageBucket: "holy-counseling-aid.appspot.com",
+  storageBucket: "holy-counseling-aid.firebasestorage.app",
   messagingSenderId: "177143536754",
   appId: "1:177143536754:web:3b615c13ea9bf13e81f6eb"
 };
