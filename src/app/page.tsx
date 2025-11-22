16acba18e4bec1a340dec3bdd81ddd4000e0bfa8
Jeongsu Han <ggoatop98@gmail.com>
2025-11-22 15:28:53 +0000

    타이틀이 카드 테두리의 가운데 배치. (_for element <CardHeader>_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -185,7 +185,7 @@ function HomePageContent() {
           <div className="space-y-8 md:col-span-1">
              <Card>
               <CardHeader>
-                <CardTitle>빠른 실행</CardTitle>
+                <CardTitle className="text-xl text-center">빠른 실행</CardTitle>
               </CardHeader>
               <CardContent className="flex flex-col sm:flex-row gap-4 p-4">
                 <Button onClick={() => setIsAppointmentModalOpen(true)} className="w-full sm:w-auto flex-grow">
