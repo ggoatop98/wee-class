f4a95d466a66a7bb86796c9b5f5c88bc4f929095
Jeongsu Han <ggoatop98@gmail.com>
2025-12-26 14:53:09 +0000

    자 그럼 이제부터 하나씩 수정하자. 1번 문제 해결해

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzS0kGNOaEcKKAWN5Bhk37W8c8i7aZghs",
-  authDomain: "holy-counseling-aid.firebaseapp.com",
-  projectId: "holy-counseling-aid",
-  storageBucket: "holy-counseling-aid.firebasestorage.app",
-  messagingSenderId: "177143536754",
-  appId: "1:177143536754:web:3b615c13ea9bf13e81f6eb"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
